266ee35f2847ee57112fea20b1de864c5700e12a
Tom Weiske <tweiske+github.com@cweiske.de>
2021-01-23 15:11:25 +0100

    Erster Test

---
+++ 2d-rpg-tileset.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="2d-rpg-tileset" tilewidth="32" tileheight="32" tilecount="300" columns="20">
+ <properties>
+  <property name="collides" type="bool" value="true"/>
+ </properties>
+ <image source="2d-rpg-tileset.png" width="640" height="480"/>
+</tileset>
